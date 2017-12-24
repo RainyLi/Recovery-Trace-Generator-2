d34933d26d1e8d938c54535d8a1103d63987a343
liluyurainy <liluyurainy@163.com>
2017-12-24 17:21:28 +0800

    rdp evenodd and xcode checked no_cache_2 checked trace_generator_2 updated

--- trace_generator_2.py
+++ trace_generator_2.py
@@ -5,44 +5,39 @@ from ARC_cache import *
 from LFU_cache import *
 from LRU_cache import *
 from FIFO_cache import *
-from NO_cache import *
+from NO_cache_2 import *
+
 import os
 
-dir_path = os.path.dirname(os.path.realpath(__file__)) + "\\trace\\"
+dir_path = os.path.dirname(os.path.realpath(__file__)) + "/trace/"
 #f_metric = open(dir_path+"hitRatio_and_diskIO.csv", 'w')
-code_dic={1:"star",2:"triple_star",3:"triple_parity",4:"tip",5:"hdd1"}
+code_dic={1:"star",2:"triple_star",3:"triple_parity",4:"tip",5:"hdd1", 6:"evenodd", 7:"rdp", 8:"xcode"}
 #f_metric.write("code,prime,error_disk,stripe_number,continuous_unit,cache_size,cache_method,hit_ratio\n")
 
 # maximum value
-prime_number = (13,)
+prime_number = (5,7,11,13,17)
 error_disk = 0
 stripe_number = 100
 continuous_unit = 1
 cache_size_range= (1,2,4,8,16,32,64,128)
-code_type_number= 5
 run_times=1
 scheme_comp=0
 
-# code= 1---star
-#       2---triple_star
-#       3---triple_parity
-#       4---tip
-#       5---hdd1
+# code= 6---evenodd
+#       7---rdp
+#       8---xcode
 
-#continuous_unit = 1,2,3,...,p-1     ----normal situation
+#continuous_unit = 1,12,3,...,p-1     ----normal situation
 #                = 0                 ----random
 
-
-if (scheme_comp==0):
-    #run 1 times
-    for i in range (1, run_times+1):
+for i in range (1, run_times+1):
         f_metric = open(dir_path + "hitRatio_and_diskIO"+str(i)+".csv", 'w')
         f_metric.write("code,prime,error_disk,stripe_number,continuous_unit,cache_size,cache_method,hit_ratio,diskIO\n")
 
-        for code in range(1, code_type_number+1):
+        for code in range(6, 9):
             for prime in prime_number:
-                for continuous in range(0, prime):
-                    (parameter_prefix, disk_io, blockcount) = NO_cache_trace(code, prime, error_disk, stripe_number, continuous, dir_path, 1)
+                for continuous in range(prime-1, prime):
+                    (parameter_prefix, disk_io, blockcount) = NO_cache_trace_2(code, prime, error_disk, stripe_number, continuous, dir_path, 1)
                     f_metric.write(code_dic[code] + "," + str(prime) + "," + str(error_disk) + "," + str(stripe_number) + "," + str(continuous) + ",0,-,0,"+str(disk_io)+"\n")
 
                     for cache_size in cache_size_range:
@@ -76,34 +71,4 @@ if (scheme_comp==0):
 
         f_metric.close()
 
-else:
-    # run 1 times
 
-    #scheme == 0: conventional scheme
-    #       == 1: FBF scheme
-    dir_path = os.path.dirname(os.path.realpath(__file__)) + "\\SchemeCompTrace\\"
-    for i in range(1, run_times + 1):
-        f_metric = open(dir_path + "scheme_blockcount" + str(i) + ".csv", 'w')
-        f_metric.write("code,prime,error_disk,stripe_number,continuous_unit,scheme,blockcount\n")
-
-        for code in range(1, code_type_number + 1):
-            for prime in prime_number:
-                for continuous in range(1, prime):
-
-                    #conventional scheme
-                    print("i=" + str(i) + " code=" + str(code) + " prime=" + str(prime) + " continuous=" + str(
-                        continuous)  + " scheme=0\n")
-                    (parameter_prefix, disk_io, blockcount) = NO_cache_trace(code, prime, error_disk, stripe_number, continuous,
-                                                                 dir_path, 0)
-                    f_metric.write(code_dic[code] + "," + str(prime) + "," + str(error_disk) + "," + str(
-                        stripe_number) + "," + str(continuous) + ",0," + str(blockcount) + "\n")
-
-                    #FBF scheme
-                    print("i=" + str(i) + " code=" + str(code) + " prime=" + str(prime) + " continuous=" + str(
-                        continuous) + " scheme=1\n")
-                    (parameter_prefix, disk_io, blockcount) = NO_cache_trace(code, prime, error_disk, stripe_number, continuous,
-                                                                    dir_path, 1)
-                    f_metric.write(code_dic[code] + "," + str(prime) + "," + str(error_disk) + "," + str(
-                        stripe_number) + "," + str(continuous) + ",1," + str(blockcount) + "\n")
-
-        f_metric.close()
